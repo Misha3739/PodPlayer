47580e5d0d7b360a6c0dec4ae382afe8d89ea056
Mikhail.udot <Mikhail.udot@venafi.com>
2018-04-09 23:40:52 +0300

    Change DB scripts : -migrations enabled; -drop database script added

--- DatabaseScripts/CreateDB.sql
+++ DatabaseScripts/CreateDB.sql
@@ -2,8 +2,9 @@ CREATE DATABASE PodPlayer
 GO
 USE PodPlayer
 GO
-CREATE TABLE Podcast(
-    Id bigint Identity(1,1) Primary Key,
-    Title nvarchar(50),
-    Url nvarchar(300)
-)
+--CREATE TABLE Podcast(
+--   Id bigint Identity(1,1) Primary Key,
+--   Title nvarchar(50),
+--    Url nvarchar(300)
+--)
+--ALTER DATABASE IMOLTP COLLATE Latin1_General_100_CI_AS  
