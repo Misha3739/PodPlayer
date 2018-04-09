2e987bd4e246b2faed9175d806653edbb5ce3194
Mikhail.udot <Mikhail.udot@venafi.com>
2018-04-09 22:22:02 +0300

    Replace connection string to app.config

--- PodPlayer/Storage/PodcastsDBContext.cs
+++ PodPlayer/Storage/PodcastsDBContext.cs
@@ -2,12 +2,18 @@
 using System;
 using PodPlayer.Models;
 using System.Data.Common;
+using System.Configuration;
 
 namespace PodPlayer.Storage
 {
     public class PodcastsDBContext : DbContext
     {
-        private static readonly string _connectionString = @"Server=localhost;Database=PodPlayer;User Id=sa;Password=Fergana12;";
+        private static readonly string _connectionString;
+
+        static PodcastsDBContext()
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["PodplayerConnection"].ConnectionString;
+        }
 
         public PodcastsDBContext() : base(_connectionString)
         {
